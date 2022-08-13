cbc254db1bb6910d7aa9974c60c403116a11d384
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2022-08-13 15:39:04 -0300

    Agrego Login con chequeo de campos

---
+++ workspace-inicial/js/login.js
@@ -0,0 +1,50 @@
+let form = document.forms['loginForm']
+
+const validateForm = () => {
+    let emailInput = form['emailInput'].value
+    let passwordInput = form['passwordInput'].value
+    
+    if(passwordInput === '' && emailInput === '') {
+        showAlertError('El email y la contraseña no pueden ser vacios')
+        setErrorOutput('passwordInput')
+        setErrorOutput('emailInput')
+        return false
+    } else {
+        if(passwordInput !== ''){
+            if(emailInput !== ''){
+                if(passwordInput >= 6) {
+                    console.log('todo ok')
+                } else {
+                    setErrorOutput('passwordInput')
+                    showAlertError('La contraseña tiene que tener mas de 6 caracteres')
+                    return false
+                }
+            } else {
+                setErrorOutput('emailInput')
+                showAlertError('El email no puede ser vacio')
+                return false
+            }
+        } else {
+            setErrorOutput('passwordInput')
+            showAlertError('La contraseña no puede ser vacia')
+            return false
+        }
+    }
+}
+
+const setErrorOutput = (element) => {
+    form[element].classList.add('error-input')
+    setTimeout(() => {
+        form[element].classList.remove('error-input')
+    }, 5000)
+}
+
+const showAlertError = (text) => {
+    let alert = document.getElementById("alert-danger")
+    alert.innerHTML += `<p id='text-for-danger'>${text}</p>`
+    alert.classList.add('show')
+    setTimeout(() => {
+        alert.classList.remove('show')
+        alert.removeChild(document.getElementById('text-for-danger'))
+    }, 5000)    
+}

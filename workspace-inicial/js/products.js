2487669acc36de4ad9ee719da4dd925f7f154a76
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2022-08-17 23:16:46 -0300

    Agrego listado de autos

--- workspace-inicial/js/products.js
+++ workspace-inicial/js/products.js
@@ -1,1 +1,31 @@
-console.log(getJSONData(PRODUCTS_URL + '101.json'))
+let container = document.getElementById('main-container')
+
+getJSONData(PRODUCTS_URL + '101.json')
+.then(response => {
+    console.log(response)
+    for(let object in response.data.products){
+        makeCardsForResponse(response.data.products[object])
+    }
+})
+
+const makeCardsForResponse = (data) => {
+    console.log(data)
+    let card = `
+    <div class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col-3">
+                <img src="../${data.image}" alt="${data.description}" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">${data.name}</h4>
+                </div>
+                <p class="mb-1">${data.description}</p>
+                <p class="mb-2"> <b>Precio: </b>U$D${data.cost}</p>
+            </div>
+        </div>
+    </div>
+    `
+    container.innerHTML += card
+}
+

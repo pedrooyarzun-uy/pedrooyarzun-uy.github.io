c6ee6676b242779a903915d658c549bd91321393
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2022-09-03 16:36:55 -0300

    Filtrado de productos completo

--- workspace-inicial/js/products.js
+++ workspace-inicial/js/products.js
@@ -1,19 +1,8 @@
 let container = document.getElementById('main-container')
 
-getJSONData(PRODUCTS_URL + localStorage.getItem("catID") + ".json")
-.then(response => {
-    for(let object in response.data.products){
-        makeCardsForResponse(response.data.products[object])
-    }
-    let textTitle = document.createTextNode(response.data.catName)
-    let textDescription = document.createTextNode("Aqui veras todos los " + response.data.catName.toLowerCase() + " disponibles a la venta")
-    document.getElementById("titleContent").appendChild(textTitle)
-    document.getElementById("titleDescription").appendChild(textDescription)
-})
-
 const makeCardsForResponse = (data) => {
     let card = `
-    <div class="list-group-item list-group-item-action">
+    <div class="list-group-item list-group-item-action products-cards">
         <div class="row">
             <div class="col-3">
                 <img src="${data.image}" alt="${data.description}" class="img-thumbnail">
@@ -32,5 +21,99 @@ const makeCardsForResponse = (data) => {
     container.innerHTML += card
 }
 
-let title = document.getElementById("titleContent")
-let text = document.createTextNode()
+document.addEventListener("DOMContentLoaded", function(e){
+    
+    getJSONData(PRODUCTS_URL + localStorage.getItem("catID") + ".json")
+    .then(response => {
+        localStorage.setItem('productos', JSON.stringify(response.data.products))        
+        for(let object in response.data.products){
+            makeCardsForResponse(response.data.products[object])
+        }
+        let textTitle = document.createTextNode(response.data.catName)
+        let textDescription = document.createTextNode("Aqui veras todos los " + response.data.catName.toLowerCase() + " disponibles a la venta")
+        document.getElementById("titleContent").appendChild(textTitle)
+        document.getElementById("titleDescription").appendChild(textDescription)
+        
+    })
+
+
+})
+
+const deleteDisplayedCards = () => {
+    const displayedCards = document.querySelectorAll(".products-cards");
+    for(let card of displayedCards){
+        card.remove()
+    }
+}
+
+const filterByDesc = () => {
+    deleteDisplayedCards()
+    let cards = JSON.parse(localStorage.getItem("productos"))
+    cards.sort((a, b) => a.cost - b.cost);
+    for (let card in cards){
+        makeCardsForResponse(cards[card])
+    }
+}
+
+const filterByAsc = () => {
+    deleteDisplayedCards()
+    let cards = JSON.parse(localStorage.getItem("productos"))
+    cards.sort((a, b) => b.cost - a.cost)
+    for (let card in cards){
+        makeCardsForResponse(cards[card])
+    }
+}
+
+const filterByRange = () => {
+    let rangeMinimum = document.getElementById('rangeFilterCountMin').value
+    let rangeMaximum = document.getElementById('rangeFilterCountMax').value
+    
+    if(rangeMaximum == '' && rangeMinimum == ''){
+        return showAlertError('Minimo y maximo estan vacios')
+    }
+
+    if(rangeMaximum == ''){
+        return showAlertError('Maximo esta vacio')
+    }
+    
+    if (rangeMinimum == ''){
+        return showAlertError('Minimo esta vacio')
+    }
+    
+    deleteDisplayedCards()
+    let cards = JSON.parse(localStorage.getItem("productos"))
+    let cardCounter = 0
+    for(let card in cards){
+        if(cards[card].cost <= rangeMaximum && cards[card].cost >= rangeMinimum){
+            cardCounter++
+            makeCardsForResponse(cards[card])
+        }
+    }
+
+    if(cardCounter === 0){
+        for (let card in cards){
+            makeCardsForResponse(cards[card])
+        }
+        showAlertError('No se encontraron productos en ese rango de precio')
+    }
+}
+
+const clearFilters = () => {
+    deleteDisplayedCards()
+    let cards = JSON.parse(localStorage.getItem("productos"))
+    for (let card in cards) {
+        makeCardsForResponse(cards[card])
+    }
+    document.getElementById('rangeFilterCountMin').value = ''
+    document.getElementById('rangeFilterCountMax').value =''
+}
+
+const showAlertError = (text) => {
+    let alert = document.getElementById("alert-danger")
+    alert.innerHTML += `<p id='text-for-danger'>${text}</p>`
+    alert.classList.add('show')
+    setTimeout(() => {
+        alert.classList.remove('show')
+        alert.removeChild(document.getElementById('text-for-danger'))
+    }, 5000)    
+}

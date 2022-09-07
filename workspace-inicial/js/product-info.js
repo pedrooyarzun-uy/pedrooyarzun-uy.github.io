d4eec01a7dede898b572ba8576d4793d9babda8b
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2022-09-07 15:54:52 -0300

    Agrego cajas para comentarios y controles para enviar comentarios

--- workspace-inicial/js/product-info.js
+++ workspace-inicial/js/product-info.js
@@ -1,17 +1,12 @@
-var name = document.getElementById("name")
-var description = document.getElementById("description")
-var category = document.getElementById("category")
-var sellQuantity = document.getElementById("sellQuantity")
 
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_URL + localStorage.getItem("productId") + EXT_TYPE)
     .then(response => {
-        laodProduct(response.data)
-        
+        loadProduct(response.data)
     })
 })
 
-const laodProduct = (data) => {
+const loadProduct = (data) => {
     let name = document.createTextNode(data.name)
     let description = document.createTextNode(data.description)
     let category = document.createTextNode(data.category)
@@ -24,8 +19,6 @@ const laodProduct = (data) => {
     document.getElementById("price").appendChild(price)
     document.getElementById("sellQuantity").appendChild(sellQuantity)
     document.getElementById("card-header").appendChild(name)
-
-
 
     let carouselDiv = document.getElementById("carousel-inner")
     for(let image in data.images){
@@ -45,4 +38,47 @@ const laodProduct = (data) => {
         }
         carouselDiv.innerHTML += product
     }
+}
+
+getJSONData(PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("productId") + EXT_TYPE)
+.then(response => {
+    loadComments(response.data)
+})
+
+const loadComments = (data) => {
+    for (let comment in data){
+        makeCommentCards(data[comment])
+    }
+}
+
+const makeCommentCards = (data) => {
+    let containerForComments = document.getElementById("comments-section-start") 
+    
+    let card = `
+    <div class="card text-dark bg-light mb-3" style="margin-top: 0px;">
+        <div class="card-header" id="user-name">
+            <i class="fas fa-user"></i>
+            ${data.user}
+        </div>
+        <div class="card-body">
+            <h5 class="card-title" id="stars-section"> ${asignStars(data.score)} </h5>
+            <small class="text-muted">${data.dateTime}</small>
+            <p class="card-text" id="comment-description">${data.description}</p>
+        </div>
+    </div>
+    `
+
+    containerForComments.innerHTML += card
+}
+
+
+
+const asignStars = (stars) => {
+    let totalStars = ''
+    
+    for(let x = 0; x< parseInt(stars); x++){
+         totalStars += `<span class="fa fa-star checked"></span>`
+    }
+
+    return totalStars
 }
